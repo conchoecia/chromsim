ba74dc6c8720f4850a5cb697f6c273f8ff32fcf6
Arno Bluehmel <bluehmel@login01.lisc>
2023-07-11 14:02:18 +0200

    add bash file for array job execution

---
+++ inversion_sim_array.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=inversion_sim_array
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=10000M
+#SBATCH --time=0-10:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --array=1-10
+
+config=./inversion_sim_array_args
+Asize=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
+Bsize=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
+
+module load python3/3.11
+python3 inversion_sim.py $Asize $Bsize
