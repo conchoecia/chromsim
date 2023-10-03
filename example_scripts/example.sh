0a06cb5c71fca08c69bfdeee28f8f1417cd5687e
Arno Blümel <arno.bluemel@gmail.com>
2023-10-03 20:31:47 +0200

    clean up base directory

---
+++ example_scripts/example.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+    outdir=./
+else
+    outdir=$1
+fi
+
+echo simulation . . .
+./inversion_sim/main.py -o $outdir -S -a 100 -b 100 -n example
+echo plots . . .
+./inversion_sim/main.py -o $outdir -P -s $outdir/example.inv
+echo gif . . .
+./inversion_sim/main.py -o $outdir -P -s $outdir/example.inv -G
